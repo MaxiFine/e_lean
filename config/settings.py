bad4ff8d4929a4ab5020b75e8e0ca2324b85e091
MaxFine <gibboel5@gmail.com>
2023-11-18 15:59:16 +0000

    students courses enroll config

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# directing student upon login
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,7 +49,7 @@ INSTALLED_APPS = [
 
     # Local apps
     "students.apps.StudentsConfig",
-    
+
 
 ]
 
